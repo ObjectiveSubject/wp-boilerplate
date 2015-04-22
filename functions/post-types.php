cf99a4c61066b40344d877026e45aa3ab6589d48
Kirk Pettinga <kirk.pettinga@gmail.com>
2015-04-22 13:22:19 +0700

    update post-types.php

--- functions/post-types.php
+++ functions/post-types.php
@@ -1,32 +1,31 @@
 <?php
 /*
 
-// {post-type} post type ================================ /
-add_action('init', 'register_{post-type}');
+// Brands post type ================================ /
+add_action('init', 'register_brands');
 
-function register_{post-type}() {
+function register_brands() {
 
 	$labels = array (
-		'name' => '{post-type}',
-		'singular_name' => '{post-type}',
-		'menu_name' => '{post-type}',
-		'name_admin_bar' => '{post-type}',
-		'add_new' => 'Add {post-type}',
-		'add_new_item' => 'Add New {post-type}',
+		'name' => 'Brands',
+		'singular_name' => 'Brand',
+		'menu_name' => 'Brands',
+		'name_admin_bar' => 'Brand',
+		'add_new' => 'Add Brand',
+		'add_new_item' => 'Add New Brand',
 		'edit' => 'Edit',
-		'edit_item' => 'Edit {post-type}',
-		'new_item' => 'New {post-type}',
-		'all_items' => 'All {post-type}',
-		'view' => 'View {post-type}',
-		'view_item' => 'View {post-type}',
-		'search_items' => 'Search {post-type}',
-		'not_found' => 'No {post-type} Found',
-		'not_found_in_trash' => 'No {post-type} Found in Trash',
-		'parent' => 'Parent {post-type}',
+		'edit_item' => 'Edit Brand',
+		'new_item' => 'New Brand',
+		'all_items' => 'All Brands',
+		'view' => 'View Brand',
+		'view_item' => 'View Brand',
+		'search_items' => 'Search Brands',
+		'not_found' => 'No Brands Found',
+		'not_found_in_trash' => 'No Brands Found in Trash',
+		'parent' => 'Parent Brands',
 	);
 
-	register_post_type('{post-type}', array(
-			'label' => '{post-type}',
+	register_post_type('brands', array(
 			'description' => '',
 			'public' => true,
 			'show_ui' => true,
@@ -34,9 +33,9 @@ function register_{post-type}() {
 			'capability_type' => 'post',
 			'map_meta_cap' => true,
 			'hierarchical' => false,
-			'rewrite' => array('slug' => '{post-type}', 'with_front' => true),
+			'rewrite' => array('slug' => 'brands', 'with_front' => false),
 			'query_var' => true,
-			'menu_position' => '0',
+			'menu_position' => 5,
 			'supports' => array('title','editor','thumbnail'),
 			'labels' => $labels
 		)
